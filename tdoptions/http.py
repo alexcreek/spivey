b09b74197e107e02990c4f6e7a99532d434eed33
Alex Creek <me@alexcreek.com>
2021-12-10 19:52:41 -0500

    add get and post wrappers

--- tdoptions/http.py
+++ tdoptions/http.py
@@ -1,12 +1,55 @@
+import sys
+from time import sleep
+import requests
 
-def get():
+def get(*args, **kwargs):
     """
-    Make a GET request with a timeout
-    """
-    pass
+    Wrapper around requests.get() with retries and exception handling.
 
-def post():
+    :param *args: Args that requests.get() takes
+    :param **kwargs: Keyword args that requests.get() takes
+    :return: The request.get() response object
+    :rtype: requests.models.Response
     """
-    Make a POST request with a timeout
+
+    retries = 3
+    backoff = 0
+    while retries > 0:
+        try:
+            resp = requests.get(*args, **kwargs)
+            resp.raise_for_status() # Raise HTTPError for 4xx and 5xx responses
+        except (requests.exceptions.Timeout, requests.exceptions.HTTPError) as e:
+            #TODO use a logger
+            print(e)
+            if retries == 1:
+                sys.exit(1)
+        retries -= 1
+        backoff += 1
+        sleep(backoff)
+    return resp
+
+def post(*args, **kwargs):
     """
-    pass
+    Wrapper around requests.post() with retries and exception handling.
+
+    :param *args: Args that requests.post() takes
+    :param **kwargs: Keyword args that requests.post() takes
+    :return: The request.post() response object
+    :rtype: requests.models.Response
+    """
+
+    retries = 3
+    backoff = 0
+    while retries > 0:
+        try:
+            resp = requests.post(*args, **kwargs)
+            resp.raise_for_status() # Raise HTTPError for 4xx and 5xx responses
+        except (requests.exceptions.Timeout, requests.exceptions.HTTPError) as e:
+            #TODO use a logger
+            print(e)
+            if retries == 1:
+                sys.exit(1)
+        retries -= 1
+        backoff += 1
+        sleep(backoff)
+    return resp
--- tests/test_http.py
+++ tests/test_http.py
@@ -1,22 +1,43 @@
+from contextlib import suppress
 import pytest
-from tdoptions import auth
-
-# https://docs.pytest.org/en/6.2.x/getting-started.html
-# https://docs.pytest.org/en/6.2.x/monkeypatch.html
-# https://docs.pytest.org/en/6.2.x/fixture.html
-# https://docs.pytest.org/en/6.2.x/capture.html
+import pytest_httpbin
+from tdoptions import http
 
 class TestGet:
-    def test_timing_out(self):
-        assert False
+    def test_timing_out(self, httpbin):
+        with pytest.raises(SystemExit):
+            http.get(f'{httpbin.url}/delay/2', timeout=1)
 
-    def test_getting_rate_limited(self):
-        assert False
+    def test_getting_rate_limited(self, httpbin):
+        with pytest.raises(SystemExit):
+            http.get(f'{httpbin.url}/status/429')
+
+    @pytest.mark.parametrize('status', ['404', '502'])
+    def test_4xx_and_5xx_responses(self, httpbin, status):
+        with pytest.raises(SystemExit):
+            http.get(f'{httpbin.url}/status/{status}')
+
+    def test_retries(self, httpbin, capsys):
+        with suppress(SystemExit):
+            http.get(f'{httpbin.url}/status/504')
+        assert capsys.readouterr().out.count('GATEWAY TIMEOUT') == 3
 
 
 class TestPost:
-    def test_timing_out(self):
-        assert False
+    def test_timing_out(self, httpbin):
+        with pytest.raises(SystemExit):
+            http.post(f'{httpbin.url}/delay/2', timeout=1)
 
-    def test_getting_rate_limited(self):
-        assert False
+    def test_getting_rate_limited(self, httpbin):
+        with pytest.raises(SystemExit):
+            http.post(f'{httpbin.url}/status/429')
+
+    @pytest.mark.parametrize('status', ['404', '502'])
+    def test_4xx_and_5xx_responses(self, httpbin, status):
+        with pytest.raises(SystemExit):
+            http.post(f'{httpbin.url}/status/{status}')
+
+    def test_retries(self, httpbin, capsys):
+        with suppress(SystemExit):
+            http.post(f'{httpbin.url}/status/504')
+        assert capsys.readouterr().out.count('GATEWAY TIMEOUT') == 3
