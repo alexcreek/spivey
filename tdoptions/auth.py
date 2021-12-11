ff9e2abd61827f331eb546cf91ab6bad0d05d9f6
Alex Creek <me@alexcreek.com>
2021-12-11 02:34:46 -0500

    create Auth class

--- tdoptions/auth.py
+++ tdoptions/auth.py
@@ -2,46 +2,50 @@ import os
 import sys
 from .http import post
 
-def token():
-    """
-    Generate a bearer token returned as a string.
+class Auth():
+    def __init__(self):
+        # load creds
+        try:
+            self.refresh_token = os.environ['REFRESH_TOKEN']
+            self.client_id = os.environ['CLIENT_ID']
+        except KeyError as e:
+            print(f'{e} environment variable not found')
+            sys.exit(1)
 
-    CLIENT_ID and REFRESH_TOKEN environment variables are used
-    to auth
-    :return: Bearer token
-    :rtype: string
-    """
+        self.payload = {
+            'grant_type': 'refresh_token',
+            'refresh_token': self.refresh_token,
+            'client_id': self.client_id
+        }
 
-    # load creds
-    try:
-        refresh_token = os.environ['REFRESH_TOKEN']
-        client_id = os.environ['CLIENT_ID']
-    except KeyError as e:
-        print(f'{e} environment variable not found')
-        sys.exit(1)
+        # load settings
+        self.auth_url = os.getenv('TD_AUTH_URL', 'https://api.tdameritrade.com/v1/oauth2/token')
+        self.timeout  = int(os.getenv('TD_TIMEOUT', '30'))
 
-    payload = {'grant_type': 'refresh_token',
-               'refresh_token': refresh_token,
-               'client_id': client_id}
+    def token(self):
+        """
+        Generate a bearer token returned as a string.
 
-    # load settings
-    auth_url = os.getenv('TD_AUTH_URL', 'https://api.tdameritrade.com/v1/oauth2/token')
-    timeout  = int(os.getenv('TD_TIMEOUT', '30'))
+        CLIENT_ID and REFRESH_TOKEN environment variables are used
+        to auth
+        :return: Bearer token
+        :rtype: string
+        """
 
-    resp = post(auth_url, data=payload, timeout=timeout)
-    try:
-        _token = resp.json()['access_token']
-    except KeyError as e:
-        print(f'{e} missing from response')
-        sys.exit(1)
-    return _token
+        resp = post(self.auth_url, data=self.payload, timeout=self.timeout)
+        try:
+            _token = resp.json()['access_token']
+        except KeyError as e:
+            print(f'{e} missing from response')
+            sys.exit(1)
+        return _token
 
-def header():
-    """
-    Generate a bearer token returned in Authorization header format.
+    def header(self):
+        """
+        Generate a bearer token returned in Authorization header format.
 
-    :return: HTTP Authorization header
-    :rtype: dict
-    """
+        :return: HTTP Authorization header
+        :rtype: dict
+        """
 
-    return {'Authorization': 'Bearer ' + token()}
+        return {'Authorization': 'Bearer ' + self.token()}
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,29 +1,52 @@
 import pytest
-from tdoptions import auth
+import requests
+from tdoptions.auth import Auth
 
-# https://docs.pytest.org/en/6.2.x/getting-started.html
-# https://docs.pytest.org/en/6.2.x/monkeypatch.html
-# https://docs.pytest.org/en/6.2.x/fixture.html
-# https://docs.pytest.org/en/6.2.x/capture.html
+class MockResponse:
+    status_code = 200
+
+    @staticmethod
+    def json():
+        return {'access_token': 'asdf'}
+
 
 class TestAuth:
-    def test_missing_refresh_token(self):
-        assert False
+    def test_missing_refresh_token(self, monkeypatch):
+        monkeypatch.setenv('CLIENT_ID', 'asdf')
+        with pytest.raises(SystemExit):
+            a = Auth()
+            a.header()
 
-    def test_missing_client_id(self):
-        assert False
+    def test_missing_client_id(self, monkeypatch):
+        monkeypatch.setenv('REFRESH_TOKEN', 'asdf')
+        with pytest.raises(SystemExit):
+            a = Auth()
+            a.header()
 
-    def test_invalid_token(self):
-        assert False
+    def test_token_missing_from_response(self, monkeypatch, httpbin):
+        monkeypatch.setenv('TD_AUTH_URL', f'{httpbin.url}/status/200')
+        with pytest.raises(SystemExit):
+            a = Auth()
+            a.header()
 
-    def test_invalid_client_id(self):
-        assert False
+    def test_token_as_a_header(self, monkeypatch):
+        def mock_post(*args, **kwargs):
+            return MockResponse()
 
-    def test_timing_out(self):
-        assert False
+        monkeypatch.setattr(requests, "post", mock_post)
+        monkeypatch.setenv('CLIENT_ID', 'asdf')
+        monkeypatch.setenv('REFRESH_TOKEN', 'asdf')
 
-    def test_getting_rate_limited(self):
-        assert False
+        a = Auth()
+        assert a.header() == {'Authorization': 'Bearer asdf'}
 
-    def test_success(self):
-        assert False
+    def test_token_as_a_string(self, monkeypatch):
+        def mock_post(*args, **kwargs):
+            return MockResponse()
+
+        monkeypatch.setattr(requests, "post", mock_post)
+        monkeypatch.setenv('CLIENT_ID', 'asdf')
+        monkeypatch.setenv('REFRESH_TOKEN', 'asdf')
+
+        a = Auth()
+        assert a.token() == 'asdf'
